f482f9cf57214b1ac553325464296c08a19be289
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-24 18:36:01 -0400

    Create AutoClicker.java

---
+++ HydraX/src/Hacks/Combat/AutoClicker.java
@@ -0,0 +1,19 @@
+public class AutoClicker {
+    private int clickRate = 10; // clicks per second
+
+    public void setClickRate(int rate) {
+        this.clickRate = rate;
+    }
+
+    public int getClickRate() {
+        return clickRate;
+    }
+
+    public void startClicking() {
+        // Logic to automatically click at the specified rate
+    }
+
+    public void stopClicking() {
+        // Logic to stop clicking
+    }
+}
