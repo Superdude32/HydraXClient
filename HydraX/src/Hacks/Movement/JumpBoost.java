91f82d84cb133528901c09b30119fa828f8a9ad6
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-24 18:33:15 -0400

    Create JumpBoost.java

---
+++ HydraX/src/Hacks/Movement/JumpBoost.java
@@ -0,0 +1,11 @@
+public class JumpBoost {
+    private float jumpHeight = 2.0f;
+
+    public void setJumpHeight(float height) {
+        this.jumpHeight = height;
+    }
+
+    public float getJumpHeight() {
+        return jumpHeight;
+    }
+}
