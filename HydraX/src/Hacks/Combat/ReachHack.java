98eb258358759c539beb1963b7f35d945d1f4365
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-24 18:36:34 -0400

    Create ReachHack.java

---
+++ HydraX/src/Hacks/Combat/ReachHack.java
@@ -0,0 +1,11 @@
+public class ReachHack {
+    private float reachDistance = 6.0f;
+
+    public void setReach(float distance) {
+        this.reachDistance = distance;
+    }
+
+    public float getReach() {
+        return reachDistance;
+    }
+}
