80b7bf1b0fabd215f212dde4c426323322f92b7c
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-24 18:35:26 -0400

    Create Aimbot.java

---
+++ HydraX/src/Hacks/Combat/Aimbot.java
@@ -0,0 +1,9 @@
+public class Aimbot {
+    public void enable() {
+        // Logic to automatically aim at targets
+    }
+
+    public void disable() {
+        // Logic to stop aiming automatically
+    }
+}
