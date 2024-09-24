42d8911427d264dbc3162c52ed94af66e5aa1e5a
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-24 18:30:12 -0400

    Create NoFallDamage.java

---
+++ HydraX/src/Hacks/Movement/NoFallDamage.java
@@ -0,0 +1,9 @@
+public class NoFallDamage {
+    public void enable() {
+        // Logic to prevent fall damage
+    }
+
+    public void disable() {
+        // Logic to allow fall damage
+    }
+}
