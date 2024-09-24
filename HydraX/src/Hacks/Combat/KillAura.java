853f434a35696956488fe036785f608281625557
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-24 18:34:52 -0400

    Update and rename tmp to KillAura.java

---
+++ HydraX/src/Hacks/Combat/KillAura.java
@@ -0,0 +1,9 @@
+public class KillAura {
+    public void enable() {
+        // Logic to automatically attack nearby enemies
+    }
+
+    public void disable() {
+        // Logic to stop attacking
+    }
+}
