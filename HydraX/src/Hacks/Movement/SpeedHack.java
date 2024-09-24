649d279466de37f0ae1cd238fe5e5ed8cc1a9965
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-24 18:29:13 -0400

    Create SpeedHack.java

---
+++ HydraX/src/Hacks/Movement/SpeedHack.java
@@ -0,0 +1,11 @@
+public class SpeedHack {
+    private boolean enabled = false;
+
+    public void toggle() {
+        enabled = !enabled;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
