f48ecc98a9f8a086136437a94df05e95ef87eacf
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-24 18:20:11 -0400

    Update HydraX.java

--- HydraX/src/HydraX.java
+++ HydraX/src/HydraX.java
@@ -1,1 +1,8 @@
-
+public class HydraX {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        // Key listener to toggle menu
+        KeyListener keyListener = new KeyListener(menu);
+        // Game loop or event loop...
+    }
+}
