d4d9b60480920cd4ea72510d84f564b2d6ab2bd2
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:46:18 -0400

    Create WhitelistBlacklist.java

---
+++ HydraX/src/Hacks/Miscellaneous/WhitelistBlacklist.java
@@ -0,0 +1,9 @@
+public class WhitelistBlacklist {
+    public void addToWhitelist(String playerName) {
+        // Logic to add a player to the whitelist
+    }
+
+    public void addToBlacklist(String playerName) {
+        // Logic to add a player to the blacklist
+    }
+}
