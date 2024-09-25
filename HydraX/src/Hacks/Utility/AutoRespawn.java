2a9fd1ea5647958dd6eb02fc76cb2487da99fdf9
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:37:24 -0400

    Update and rename tmp to AutoRespawn.java

---
+++ HydraX/src/Hacks/Utility/AutoRespawn.java
@@ -0,0 +1,9 @@
+public class AutoRespawn {
+    public void enable() {
+        // Logic to automatically respawn after death
+    }
+
+    public void disable() {
+        // Logic to stop auto-respawn
+    }
+}
