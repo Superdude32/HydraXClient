46c35fbf1247d1b84b589e1ccc196af5e2100509
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:45:03 -0400

    Create BlockPlacement.java

---
+++ HydraX/src/Hacks/WorldManipulation/BlockPlacement.java
@@ -0,0 +1,9 @@
+public class BlockPlacement {
+    public void enable() {
+        // Logic to automatically place blocks
+    }
+
+    public void disable() {
+        // Logic to stop placing blocks
+    }
+}
