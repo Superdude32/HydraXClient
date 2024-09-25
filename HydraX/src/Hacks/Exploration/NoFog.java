b210b62b6263a6afa5f486428cec486c3492fc9f
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:43:58 -0400

    Update and rename tmp to NoFog.java

---
+++ HydraX/src/Hacks/Exploration/NoFog.java
@@ -0,0 +1,9 @@
+public class NoFog {
+    public void enable() {
+        // Logic to remove fog for better visibility
+    }
+
+    public void disable() {
+        // Logic to restore fog
+    }
+}
