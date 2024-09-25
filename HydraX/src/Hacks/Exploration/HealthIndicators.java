897ab86e93201b98a78c454d00dd092ea878dcca
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:36:50 -0400

    Create HealthIndicators.java

---
+++ HydraX/src/Hacks/Exploration/HealthIndicators.java
@@ -0,0 +1,9 @@
+public class HealthIndicators {
+    public void enable() {
+        // Logic to display health of nearby players
+    }
+
+    public void disable() {
+        // Logic to stop displaying health
+    }
+}
