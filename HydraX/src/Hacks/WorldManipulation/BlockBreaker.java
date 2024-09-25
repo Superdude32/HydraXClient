987b1bfc53c1213bac555f5140a3ba884b612788
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:44:47 -0400

    Update and rename tmp to BlockBreaker.java

---
+++ HydraX/src/Hacks/WorldManipulation/BlockBreaker.java
@@ -0,0 +1,9 @@
+public class BlockBreaker {
+    public void enable() {
+        // Logic to automatically break nearby blocks
+    }
+
+    public void disable() {
+        // Logic to stop breaking blocks
+    }
+}
