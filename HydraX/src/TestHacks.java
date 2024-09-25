1db0e3efa344554351350f8e4fe8cb29e5ee0e6d
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:51:58 -0400

    Create TestHacks.java

---
+++ HydraX/src/TestHacks.java
@@ -0,0 +1,9 @@
+public class TestHacks {
+    public static void main(String[] args) {
+        // Example test for SpeedHack
+        SpeedHack speedHack = new SpeedHack();
+        System.out.println("SpeedHack instantiated: " + (speedHack != null));
+        
+        // Add additional tests for other hacks
+    }
+}
