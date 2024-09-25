d8763650df9ff84a12914985997a6aec0cb4abb4
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:37:42 -0400

    Create AutoInventoryManagement.java

---
+++ HydraX/src/Hacks/Utility/AutoInventoryManagement.java
@@ -0,0 +1,9 @@
+public class AutoInventoryManagement {
+    public void enable() {
+        // Logic to automatically sort inventory
+    }
+
+    public void disable() {
+        // Logic to stop automatic sorting
+    }
+}
