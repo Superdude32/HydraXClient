4cff4aaec58c84705db87599419ffe2ac89ac062
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:38:38 -0400

    Create FlySpeedControl.java

---
+++ HydraX/src/Hacks/Utility/FlySpeedControl.java
@@ -0,0 +1,11 @@
+public class FlySpeedControl {
+    private float flySpeed = 1.0f;
+
+    public void setFlySpeed(float speed) {
+        this.flySpeed = speed;
+    }
+
+    public float getFlySpeed() {
+        return flySpeed;
+    }
+}
