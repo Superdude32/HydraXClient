91f2e9cfa0a120dc2ce8302e099ba5db0f26704e
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:35:49 -0400

    Update ESP.java

--- HydraX/src/Hacks/Exploration/ESP.java
+++ HydraX/src/Hacks/Exploration/ESP.java
@@ -1,1 +1,9 @@
+public class ESP {
+    public void enable() {
+        // Logic to highlight players and items through walls
+    }
 
+    public void disable() {
+        // Logic to stop highlighting
+    }
+}
