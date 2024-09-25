8df01316481bc134bbfc32f2a527070e32c90126
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:45:50 -0400

    Update and rename tmp to ChatSpam.java

---
+++ HydraX/src/Hacks/Miscellaneous/ChatSpam.java
@@ -0,0 +1,7 @@
+public class ChatSpam {
+    public void spam(String message, int times) {
+        for (int i = 0; i < times; i++) {
+            // Logic to send a message in chat
+        }
+    }
+}
