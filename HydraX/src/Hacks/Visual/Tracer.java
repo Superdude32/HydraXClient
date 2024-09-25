d13b786754cbb61b9e35f76658c4d67104823f74
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:42:47 -0400

    Create Tracer.java

---
+++ HydraX/src/Hacks/Visual/Tracer.java
@@ -0,0 +1,9 @@
+public class Tracer {
+    public void enable() {
+        // Logic to draw lines to players or items
+    }
+
+    public void disable() {
+        // Logic to stop drawing lines
+    }
+}
