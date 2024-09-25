b6c3274e147502ab0d40e624b19d1de1557643b1
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:46:03 -0400

    Create StealthMode.java

---
+++ HydraX/src/Hacks/Miscellaneous/StealthMode.java
@@ -0,0 +1,9 @@
+public class StealthMode {
+    public void enable() {
+        // Logic to reduce visibility of hacks
+    }
+
+    public void disable() {
+        // Logic to restore visibility
+    }
+}
