242cb5dde6647db8ec53eaf8314ccb364a3a1094
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:46:44 -0400

    Create PerformanceMode.java

---
+++ HydraX/src/Hacks/Miscellaneous/PerformanceMode.java
@@ -0,0 +1,9 @@
+public class PerformanceMode {
+    public void enable() {
+        // Logic to disable heavy features for improved FPS
+    }
+
+    public void disable() {
+        // Logic to restore original settings
+    }
+}
