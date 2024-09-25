c997038d93d848176bc28e2b2260673da7d143fe
Big_Z <67666994+Superdude32@users.noreply.github.com>
2024-09-25 18:38:21 -0400

    Create AutoEat.java

---
+++ HydraX/src/Hacks/Utility/AutoEat.java
@@ -0,0 +1,9 @@
+public class AutoEat {
+    public void enable() {
+        // Logic to automatically eat food when health is low
+    }
+
+    public void disable() {
+        // Logic to stop auto-eating
+    }
+}
